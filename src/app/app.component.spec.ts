fc8391ca5cf145b7b5efbd35b4d3d1c562e5bf62
Tom <tomwansww@gmail.com>
2022-10-18 11:16:27 +0500

    Show game title instead of angular template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('game-2048 app is running!');
+    expect(compiled.querySelector('h1.game-title')?.textContent).toContain(
+      'Game 2048'
+    );
   });
 });
